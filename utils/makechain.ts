7b57b24960ea7a46ff5f2cad0ac447f3988dd8e9
Mith <mpp59@cornell.edu>
2023-07-19 00:09:45 -0400

    Huge changes. QA prompt changes entire functionality. Next steps: fix sources, implement 4 API

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,36 +2,51 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
+const CONDENSE_PROMPT = `Here is a conversation and a follow-up question. Rephrase the follow-up question to be a standalone question.
+Conversation: {chat_history}
+Follow-up: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+// SUPER IMPORTANT********************************CHANGES EVERYTHING. String to the puppet
+const QA_PROMPT = `
+As an intelligent AI, engage in a conversation, and provide an answer to 
+the question. Your responses should be accurate, detailed, and helpful. You must
+determine whether the question is related to the context, somewhat related the context, or not related to the context.
+If the question is related to the context, provide a specific answer with extreme accuracy
+by the context. If the question is somewhat related to the context, you can provide an answer to the best of your abilities
+and answer with or without the context in a accurate,detailed,and helpful manner.
+If the question is unrelated to the context completely, your response does not 
+have to be related to the context. Remember at all times you will generate an accurate,detailed,and helpful
+message regardless of the question that poses you.
 
-{context}
-
+Context: {context}
 Question: {question}
-Helpful answer in markdown:`;
+Response:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
-  const model = new OpenAI({
-    temperature: 0.1, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-  });
 
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorstore.asRetriever(),
-    {
-      qaTemplate: QA_PROMPT,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
-    },
+  // Create multiple models with different parameters
+  const models = [
+    new OpenAI({
+      temperature: 0.1, 
+      modelName: 'gpt-3.5-turbo',
+    }),
+    // Add more models with different parameters here if you want to create an ensemble
+  ];
+
+  // Create multiple chains for each model
+  const chains = models.map((model) =>
+    ConversationalRetrievalQAChain.fromLLM(
+      model,
+      vectorstore.asRetriever(),
+      {
+        qaTemplate: QA_PROMPT,
+        questionGeneratorTemplate: CONDENSE_PROMPT,
+        returnSourceDocuments: true,
+      },
+    )
   );
-  return chain;
+
+  // Implement logic to use your ensemble of chains. Here we return the first one for simplicity.
+  return chains[0];
 };
