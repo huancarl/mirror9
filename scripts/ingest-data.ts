c02cd7438aae141ac78dd69c3bce1557e79eb527
Mith <mpp59@cornell.edu>
2023-07-24 23:40:33 -0400

    Errors! - Replication attempt to mayo

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -5,6 +5,7 @@ import { pinecone } from '@/utils/pinecone-client';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { text } from 'stream/consumers';
 
 /* Name of directory to retrieve your files from 
    Make sure to add your PDF files inside the 'docs' folder
@@ -15,17 +16,63 @@ export const run = async () => {
   try {
     /*load raw docs from the all files in the directory */
     const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new PDFLoader(path),
+      '.pdf': (path) => new PDFDistLoader(path),
     });
 
     // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+    const docs = await directoryLoader.load();
+
+    //Group documents by title
+    const groupedDocs = groupDocumentsByTitle(docs);
+
+    //console.log(rawDocs, rawDocs.length);
+    const json = JSON.stringify(Array.from(groupedDocs.entries()));
+
+    await fs.writeFile('test.json', json);
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
       chunkOverlap: 200,
     });
+
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    // Iterate through the grouped pdfs
+    for (const [title, documents] of groupedDocs.entries()) {
+      //Set namespace based on title
+      const namespace = '(title';
+
+      console.log('namespaceCount', namespace);
+
+      //Split documents into chunks
+      const splitDocs = await textSplitter.splitDocuments(documents);
+      console.log('splitDocs', splitDocs.length);
+
+      const json = JSON.stringify(splitDocs);
+
+      await fs.writeFile('(title)-split.json', json);
+
+
+      const upsertChunkSize = 50;
+      console.log('ingesting...may take a few minutes');
+      for (let i = 0; i < splitDocs.length; i += upsertChunkSize){
+        const chunk = splitDocs.slice(i, i + upsertChunkSize);
+        console.log('chunk', i, chunk);
+        await PineconeStore.fromDocuments (
+          index,
+          chunk,
+          new OpenAIEmbeddings(),
+          'text',
+          namespace
+        );
+      }
+    }
+  } catch(error) {
+    console.log('error', error);
+    throw new Error ('Failed to ingest your data cuh');
+  }
+}
 
     const docs = await textSplitter.splitDocuments(rawDocs);
     console.log('split docs', docs);
@@ -51,3 +98,4 @@ export const run = async () => {
   await run();
   console.log('ingestion complete');
 })();
+
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -14,26 +14,33 @@ Standalone question:`;
 
 // SUPER IMPORTANT********************************CHANGES EVERYTHING. String to the puppet
 const QA_PROMPT = `
-As an AI, engage in an educational conversation and provide accurate, detailed, and helpful answers to the questions asked.
+As a super-intelligent AI, engage in an educational conversation and provide accurate, detailed, and helpful answers to the questions asked.
+
+You have the ability to understand and recall information from these context. When asked a question, your job is to refer to 
+the specific content from these context to provide the most accurate,specific, and helpful response.
+
+Keep in mind that context has specific information and chapters. You should not make 
+assumptions about the content based on the chapter number alone, but rather refer to the specific content 
+of the chapter in the context.
 
 Even when a question asks for information not directly provided in the immediate context, such as details 
-about a specific chapter, use your training to provide an accurate and detailed answer where possible. 
+about a specific chapter, use your intuition to provide an accurate and detailed answer where possible. 
 When someone asks about a specific chapter, they expect a detailed and explanatory response that pertains 
-to that particular chapter. Remember you have access to all context, you just need to identify it.
+to that particular chapter. Identify context.
 
-Be consistent with your responses. If asked the same exact question twice, provide a consistent but better response.
+Be consistent, but not repetitive, with your responses. If asked the same exact question twice, provide an even better response.
 
 Always consider the relevance of the context for each individual question:
 - If a question's context is distinct from a previous one, switch context accordingly and do not carry over irrelevant information from the previous context. 
 - If the context of a question is a continuation or related to the previous context, then use the information appropriately to provide a detailed and specific response.
 
 Based on the relationship between the question and the context:
-
 - If the question is related to the context, answer precisely using the context.
-- If the question is somewhat related, provide an answer to the best of your abilities, considering the context where appropriate.
+- If the question is somewhat related, provide an answer to the best of your abilities, considering the context where appropriate, and as much as you can.
 - If the question is unrelated to the context, answer the question accurately even if the context does not provide relevant information.
 
-Any question relating to the context, provide specifically in your response where in the context they can find more information.
+When referencing specific context in your answer, like quotations, extract the specific page number and specific chapter 
+in your answer. You must give this to the user in your accurate answer. Do not repeat the same information ever.
 
 When the context is ambiguous, assume the most probable context for the question.
 
@@ -44,14 +51,14 @@ Ensure your answers are always attentive to the specifics of the question, accur
 Context: {context}
 Question: {question}
 Response:
-`;
+`; 
 
 export const makeChain = (vectorstore: PineconeStore) => {
 
   // Create multiple models with different parameters
   const models = [
     new OpenAI({
-      temperature: 0.1, 
+      temperature: 0, 
       modelName: 'gpt-3.5-turbo',
     }),
     // Add more models with different parameters here if you want to create an ensemble
@@ -61,7 +68,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
   const chains = models.map((model) =>
     ConversationalRetrievalQAChain.fromLLM(
       model,
-      vectorstore.asRetriever(),
+      vectorstore.asRetriever(5),
       {
         qaTemplate: QA_PROMPT,
         questionGeneratorTemplate: CONDENSE_PROMPT,
