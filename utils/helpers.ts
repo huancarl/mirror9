607683251ef59a2fd9338dc42001d30ee52ad05d
Carl Huang <ch976@cornell.edu>
2023-08-01 13:26:33 -0400

    Implemented helpers.ts as function for extracting textbook names to identify namespaces (currently does not match namespaces of ingested documents)

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,4 +1,15 @@
-export function extractYearsFromQuery(query) {
-    // extract years from the query and return them as an array
+export function extractYearsFromQuery(query: string) {
+    // extract name of textbooks from the query and return them as an array using regex
+
+    const pattern: RegExp = /\b(networks|game theory)\b/gi;
+    const titles: string[] = [];
+    let match;
+
+    while ((match = pattern.exec(query)) !== null) {
+      titles.push(match[1]);
+    }
+
+    return titles;
+
   }
   
