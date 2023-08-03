dbd2f0682d2ae1f18bee33db53ff7167b06fe92c
Mith <mpp59@cornell.edu>
2023-08-03 01:03:23 -0400

    Customqachain is error-less. Fix namespace naming in main.ts etc

--- utils/customqachain.ts
+++ utils/customqachain.ts
@@ -1,8 +1,10 @@
 import { OpenAIChat } from "langchain/llms";
+import axios from 'axios';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { NAMESPACE_NUMB } from "@/config/pinecone";
 import { pinecone } from "@/utils/pinecone-client";
 import { PINECONE_INDEX_NAME } from "@/config/pinecone";
+
 
 
 interface CustomQAChainOptions {
@@ -26,9 +28,10 @@ export class CustomQAChain {
       return new CustomQAChain(model, index, namespaces, options);
     }
 
-  public async call({ question, chat_history }: { question: string; chat_history: string }) {
-    // Generate the query embedding using the model.
-    const queryEmbedding = await this.model.embed(question);
+    public async call({ question, chat_history }: { question: string; chat_history: string }) {
+        // Create an instance of the OpenAIEmbeddings class.
+        const embeddings = new OpenAIEmbeddings();
+        const queryEmbedding = embeddings.embedQuery(question);
 
     // Perform a search across the specified namespaces ("book-1", "book-2", "book-3").
     const searchResults = await Promise.all(this.namespaces.map((namespace) => {
@@ -47,34 +50,39 @@ export class CustomQAChain {
 
     return formattedResults;
   }
+  
+// This probably wont even fucking work, and its okay tbh i dont think we even need it******
 
-  private parseResults(searchResults: any[]): any[] {
-    // Merge all search results.
-    return searchResults.flatMap((searchResult) => {
-      // Parse the search result to extract the required information.
-      // Adjust this line based on the structure of the data returned by Pinecone.
-      return searchResult.items; 
+private parseResults(searchResults: any[]): any[] {
+    // Flatten and merge all search results.
+    return searchResults.flatMap((searchResult, index) => {
+      // Include the book name in the parsed results.
+      return searchResult.items.map((item: any) => ({
+        ...item,
+        book: NAMESPACE_NUMB[index + 1], // Assuming index corresponds to the book key
+      }));
     });
   }
-
+  
   private async processResults(parsedResults: any[]): Promise<any> {
-    // Process the parsed results, possibly using the model for further analysis.
-    // Customize this method according to your specific requirements.
-
-    // Example: If you need further analysis by the model, you can do that here.
+    // You may process the parsed results here if needed. For example, if you need further analysis by the model, you can do that here.
+    // In this example, we simply return the parsed results.
     return parsedResults; // Adjust as needed.
   }
-
+  
   private formatResults(processedResults: any): any {
-    // Format the processed results based on the options specified in the constructor.
-
     if (this.options.returnSourceDocuments) {
-      // Return the source documents along with the results if specified.
-      // Customize this part to include the specific format you want to return.
+      // Format the results to include chapter, page number, and source document (book).
+      return processedResults.map((result: any) => ({
+        chapter: result.chapter, // Assuming this field exists in the results
+        pageNumber: result.pageNumber, // Assuming this field exists in the results
+        sourceDocument: result.book, // Book name from the namespaces
+      }));
     }
-
     return processedResults; // Adjust as needed.
-  }
+  }  
+
+  
 }
 
 
