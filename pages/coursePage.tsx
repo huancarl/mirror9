5c87f108fbfc221520f48683a985bbb6b09e7280
Carl Huang <ch976@cornell.edu>
2023-11-08 02:29:27 -0500

    Added referral to the course select page

--- pages/coursePage.tsx
+++ pages/coursePage.tsx
@@ -2,8 +2,46 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '@/styles/courseSelection.module.css';
 import CourseBox from 'components/CourseBox';
+import { v4 as uuidv4 } from 'uuid';
 
 function CourseCatalog() {
+
+  const [referralLink, setReferralLink] = useState('');
+
+  function getOrGenerateUUID(key: string): string {
+    let value = localStorage.getItem(key) || '';
+    if (!value) {
+        value = uuidv4();
+        localStorage.setItem(key, value);
+    }
+    return value;
+  }
+
+  useEffect(() => {
+    const fetchOrCreateRef = async() => {
+
+      const userID = getOrGenerateUUID('lapp');
+  
+      let link = "https://localhost.com/?ref={";
+  
+      let response = await fetch('/api/fetchOrCreateReferral', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userID,
+        }),
+      });
+      const data = await response.json();
+  
+      link = link + data.code + '}';
+      setReferralLink(link);
+    }
+    fetchOrCreateRef();
+  }, []);
+
+
   const courses = [
     { key: 'INFO 2950', title: 'INFO 2950', professor: 'Professor Koenecke' },
     { key: 'INFO 2040', title: 'INFO 2040', professor: 'Professor Easley' },
@@ -22,6 +60,7 @@ function CourseCatalog() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.referralText}>Refer a friend! <a> {referralLink} </a></div>
       <h1 className={styles.title}> CornellGPT </h1>
       <p className={styles.subtitle}>What class would you like help with?</p>
       <input
