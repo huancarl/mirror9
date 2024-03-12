2adbc2c52b58c5dd8181d1158d059551376cdbbb
huancarl <87915963+huancarl@users.noreply.github.com>
2024-03-12 16:15:58 -0400

    Update pinecone-client.ts

--- utils/pinecone-client.ts
+++ utils/pinecone-client.ts
@@ -4,17 +4,23 @@ if (!process.env.PINECONE_API_KEY) {
   throw new Error('Pinecone environment or api key vars missing');
 }
 
+let pineconeObj: any;
 async function initPinecone() {
   try {
-    const pc = new Pinecone({
+    pineconeObj = new Pinecone({
       apiKey: process.env.PINECONE_API_KEY ?? '',
     });
 
-    return pc;
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to initialize Pinecone Client');
   }
 }
 
-export const pinecone = await initPinecone();
+async function main() {
+  await initPinecone();
+}
+
+main();
+
+export const pinecone = pineconeObj;
